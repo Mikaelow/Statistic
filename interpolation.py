5fdf56147302f70310eaa3d9d1eec5b12a8152d3
Mikaelow <114190199+Mikaelow@users.noreply.github.com>
2023-10-17 00:00:33 +0200

    Finished Interpolation

--- interpolation.py
+++ interpolation.py
@@ -1,9 +1,17 @@
 import random 
 import matplotlib.pyplot as plt
-x = list(range(1,60,2))
-y=[random.randint(0,1000) for _ in range(30)]
+import numpy as np
+from scipy.interpolate import interp1d
+
+x = np.array(np.linspace(0,60,30))
+y = np.array([random.randint(0,1000) for _ in range(30)])
 plt.scatter(x,y,color='red',marker='o')
-plt.title('Standard')
+plt.legend('Standard')
+y_f=interp1d(x,y,kind='cubic')
+x = np.array(np.linspace(0,60,10000))
+y= y_f(x)
+plt.plot(x,y)
+plt.legend('Interpolation')
 plt.xlabel('x')
 plt.ylabel('y')
 plt.show()
