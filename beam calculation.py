09c05c75c90295258a220825837117ab0ae1b878
Mikaelow <114190199+Mikaelow@users.noreply.github.com>
2023-10-22 22:43:41 +0200

    proper return

--- beam calculation.py
+++ beam calculation.py
@@ -1,18 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def loop_and_cheeck(a,b,x1,x2):
-    new_x = np.linspace(x[0],x[len(x)-1],20)
-    y_f = np.array([])
-    for j in new_x:
-            if x1<=j<=x2:
-                new_y = np.round(a*j + b)
-                y_f = np.append(y_f, new_y)
-    return new_x, new_y
+def loop_and_cheeck(a,b,x1,x2,j):
+    if x1<=j<=x2:
+        new_y = np.round(a*j + b)
+        return new_y
+    else:
+        pass
 
 
 def lineal_conncection(x,y):
     i = 1
+    y_f = np.array([])
     while i < len(x):
         x1 = x[i-1] 
         x2 = x[i]
@@ -21,8 +20,13 @@ def lineal_conncection(x,y):
         indicator = indicators(x1 , x2 , y1 , y2)
         a = indicator[0]
         b = indicator[1]
-        loop_and_cheeck(a,b,x1,x2)
+        for j in new_x:
+            new_y = loop_and_cheeck(a,b,x1,x2,j)
+            y_f = np.append(y_f, new_y)
         i += 1
+    y_f = y_f[y_f != None]
+    return y_f
+    
 
 def indicators(x1 , x2 , y1 , y2):
     main_matrix = np.array([[x1,1],[x2,1]])
@@ -41,5 +45,7 @@ def coefficients_a_b(a_indicator,b_indicator,main_indicator):
 
 if __name__ == '__main__':
     x = np.array([0.3 , 0.6 , 0.8])
-    y = np.array([0   ,  10 , 50])   
-    lineal_conncection(x,y)
+    y = np.array([0   ,  10 , 50])  
+    new_x = np.linspace(x[0],x[len(x)-1],20) 
+    y_f = lineal_conncection(x,y)
+    
