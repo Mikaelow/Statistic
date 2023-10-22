2eb178879322ccb223b845d0704da14ed9ecadeb
Mikaelow <114190199+Mikaelow@users.noreply.github.com>
2023-10-22 21:54:59 +0200

    working

--- beam calculation.py
+++ beam calculation.py
@@ -2,12 +2,22 @@ import numpy as np
 
 def lineal_conncection(x,y):
     i = 1
+    y_f = np.array([])
+    new_x = np.linspace(x[0],x[len(x)-1],20)
     while i < len(x):
-        x1 = x[i-1]
+        x1 = x[i-1] 
         x2 = x[i]
-        y1 = y[i-1]
+        y1 = y[i-1] 
         y2 = y[i]
-        indicators(x1 , x2 , y1 , y2)
+        indicator = indicators(x1 , x2 , y1 , y2)
+        a = indicator[0]
+        b = indicator[1]
+        for j in new_x:
+            if x1<=j<=x2:
+                
+                new_y = np.round(a*j + b)
+                y_f = np.append(y_f, new_y)
+                print(j, new_y)
         i += 1
 
 def indicators(x1 , x2 , y1 , y2):
@@ -17,16 +27,18 @@ def indicators(x1 , x2 , y1 , y2):
     main_indicator = np.linalg.det(main_matrix)
     a_indicator = np.linalg.det(a_matrix)
     b_indicator = np.linalg.det(b_matrix)
-    coefficients_a_b(a_indicator,b_indicator,main_indicator)
+    return coefficients_a_b(a_indicator,b_indicator,main_indicator)
+
     #print(f'a = {a_indicator:.2f}, b = {b_indicator:.2f}, main = {main_indicator:.2f}')
 
 
 def coefficients_a_b(a_indicator,b_indicator,main_indicator):
     a = round(a_indicator/main_indicator,2)
     b = round(b_indicator/main_indicator,2)
-    print(f'a = {a}, b = {b}')
+    return a,b
+
 
 if __name__ == '__main__':
-    x = np.array([0.3 , 0.6 , 0.9 , 1.2 ])
-    y = np.array([0,0,590,0])   
+    x = np.array([0.3 , 0.6 , 0.8])
+    y = np.array([0   ,  10 , 50])   
     lineal_conncection(x,y)
