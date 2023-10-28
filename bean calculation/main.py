500363208ebc0b6fd1d4221aa1056c5c8c35656a
Mikaelow <114190199+Mikaelow@users.noreply.github.com>
2023-10-28 17:20:23 +0200

    add diameter fix subplot and add plot

--- bean calculation/main.py
+++ bean calculation/main.py
@@ -2,6 +2,7 @@ import numpy as np
 from BendingMoment import BendingMoment
 from Torque import Torque
 from ReducedMoment import ReducedMoment
+from Diameter import Diameter
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
@@ -16,27 +17,34 @@ if __name__ == '__main__':
         #  Bendig Moment
     bending_moment = BendingMoment(x,MGy)
     bending_f = bending_moment.bending_moment_function(new_x)
-    # bending_moment.draw_plot(new_x)
 
         #  Torque
     torque = Torque(x,MSy)
     torque_f = torque.torque_moment_function(new_x)
-    # torque.draw_plot(new_x)
 
         # Reduced Moment
-    reduced_moment = ReducedMoment(torque_f , bending_f , new_x)
+    reduced_moment = ReducedMoment(torque_f , bending_f)
     reduced_f = reduced_moment.calculate_reduce_moment()
 
+        # Diameter
+    diameter = Diameter(reduced_f)
+    diameter_f1 = diameter.calculate_diameter()
+    #diameter_f2 = [-_ for _ in diameter_f1]
+
         #Draw plots
-    plt.subplot(3,1,1)
+    plt.subplot(4,1,1)
     plt.plot(new_x, bending_f, color = 'red')
     plt.title('Bendig Moment')
-    plt.subplot(3,1,2)
+    plt.subplot(4,1,2)
     plt.plot(new_x, torque_f)
     plt.title('Torque')
-    plt.subplot(3,1,3)
+    plt.subplot(4,1,3)
     plt.plot(new_x, reduced_f, color = 'purple')
     plt.title('Reduced Moment')
+    plt.subplot(4,1,4)
+    plt.plot(new_x, diameter_f1, color = 'green')
+    #plt.plot(new_x, diameter_f2, color = 'green')
+    plt.title('Diameter')
     plt.show()
 
     
